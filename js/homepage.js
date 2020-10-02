650647c43eabf38de6a80f038ab1125f3cdba71e
arbaazshaikh <abzshaikh@gmail.com>
2020-10-03 03:16:40 +0530

    banner started

--- js/homepage.js
+++ js/homepage.js
@@ -1,3 +1,35 @@
 $(function() {
+    AOS.init({
+        delay: 500,
+        duration: 1000,
+        easing: 'ease-in-out'
+    });
+    var prevScrollpos = window.pageYOffset;
+    window.onscroll = function() {
+        var currentScrollPos = window.pageYOffset;
+        console.log(currentScrollPos +" and "+ prevScrollpos);
+        if (prevScrollpos > currentScrollPos) {
+            $("#header").css("top","0px");
+        } else {
+            $("#header").css("top","-130px");
+        }
+        prevScrollpos = currentScrollPos;
+    }
 
+    $('.banner-slider').slick({
+        dots: true,
+        infinite: true,
+        speed: 1000,
+        fade: true,
+        cssEase: 'linear',
+        autoplay: false,
+        autoplaySpeed: 3000,
+        swipeToSlide: true,
+        prevArrow: "",
+        nextArrow: "",
+        pauseOnFocus: false,
+        pauseOnHover: false
+      });
+
+      console.log("hello");
 });
